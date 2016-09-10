875a19a3bd020f47812388e02e9a5cc41dd82e33
ThomasXu18 <1714335499@qq.com>
2016-09-10 08:16:25 +0800

    2016-09-09
    将其作为开机自启的脚本（需要自己手动配置），加入了文件是否存在判断。

--- 下载必应图片/downbingpic.py
+++ 下载必应图片/downbingpic.py
@@ -1,27 +1,27 @@
 # -*- coding:utf-8 -*-
 
-"""
-author;thomas
-date: 2016-09-09
-"""
-
 import re
 import requests
 import time
 import os
 
-url = "http://cn.bing.com/"
-html = requests.get(url).text
-pic_url = re.findall(r'http://[0-9a-zA-Z\_:./-]*jpg', html)
-print(pic_url)
-try:
-    pic = requests.get(pic_url[0], timeout=10)
-except requests.exceptions.ConnectionError:
-    print("error")
-except requests.exceptionns.Timeout:
-    print("error")
-date=time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
+
+date=time.strftime('%Y-%m-%d',time.localtime(time.time()))
 string = date + '.jpg'
-fp = open(string, 'wb')
-fp.write(pic.content)
-fp.close()
+if os.path.isfile(string):
+    print("Picture already exists")
+else:
+    url = "http://cn.bing.com/"
+    html = requests.get(url).text
+    pic_url = re.findall(r'http://[0-9a-zA-Z\_:./-]*jpg', html)
+    print(pic_url)
+    try:
+        pic = requests.get(pic_url[0], timeout=10)
+    except requests.exceptions.ConnectionError:
+        print("error")
+    except requests.exceptionns.Timeout:
+        print("error")
+
+    fp = open(string, 'wb')
+    fp.write(pic.content)
+    fp.close()
